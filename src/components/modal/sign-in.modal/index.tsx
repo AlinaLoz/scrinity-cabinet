6aac455f5fd9cb83be1dee0c23f70d88b18a740d
a.lozyuk <a.lozyuk@pixelplex.io>
2021-11-07 17:02:48 +0300

    [admin-2] sign-in layout

--- src/components/modal/sign-in.modal/index.tsx
+++ src/components/modal/sign-in.modal/index.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Modal from 'react-modal';
 
+import { ModalContext, TModalData } from '@contexts/modal.context';
+import { MODAL } from '@constants/modal.constants';
+import { UrlHelper } from '@helpers/url.helper';
+import { PROJECT_NAME } from '@constants/global.constants';
+import { Input } from '@components/input/simple';
+import Button from '@components/button';
 import styles from './sign-in.module.scss';
 
-export const SignInModal: React.FC = () => (
-  <Modal
-    isOpen
-    ariaHideApp={false}
-    shouldCloseOnOverlayClick
-    className={styles.modal}
-  >
-    login
-  </Modal>
-);
+export const SignInModal: React.FC = () => {
+  const { data }: { data?: TModalData<MODAL.SIGN_IN> } = useContext(ModalContext);
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Modal
+      isOpen
+      ariaHideApp={false}
+      shouldCloseOnOverlayClick
+      className={styles.modal}
+    >
+      <div className={styles.header}>
+        <div className={styles.companyImage}>
+          <img src={UrlHelper.getImageSrc('puma.png')} alt={`company ${data?.company.id.toString() || ''}`} />
+        </div>
+        <div className={styles.projectName}>{PROJECT_NAME}</div>
+        <div className={styles.description}>Введите ваш логин и пароль</div>
+      </div>
+      <Input
+        className={styles.login}
+        placeholder="Введите логин"
+        label="логин"
+        id="login"
+        value={login}
+        onChangeValue={setLogin}
+      />
+      <Input
+        className={styles.password}
+        placeholder="Введите пароль"
+        label="пароль"
+        id="password"
+        value={password}
+        onChangeValue={setPassword}
+      />
+      <Button isFluid type="blue">Войти</Button>
+    </Modal>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,4 @@
 import React from 'react';
-import { CustomError } from '@containers/custom-error';
-import { GetServerSideProps } from 'next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { LANGUAGE } from '@constants/locales.constants';
+import { SignIn } from '@containers/sign-in';
 
-export default () => <div>Adminka</div>;
-
-export const getServerSideProps: GetServerSideProps = async (context) => ({
-  props: {
-    ...(await serverSideTranslations(context.locale || LANGUAGE.RU)),
-  },
-});
+export default () => <SignIn />;
